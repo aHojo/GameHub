3344e31f25f1db0a3ec318a447aff7d9c0f4b7f6
amh5366 <amh5366@upenn.edu>
2018-06-30 07:55:03 -0400

    added a modal

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,13 @@
-console.log("Hello World")
+var ofAge = false;
+var gameName;
+var filterLimit = 3;
+
+$(document).ready(function() {
+    $('.modal').modal('show');
+    $('#changebtn').on("click", function() {
+        ofAge = true;
+        console.log("Changed the Variables");
+    })
+
+})
+
