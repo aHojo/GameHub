7d45129023892a97eef4dc4d4e607aacd32ef760
Justin V <justinvuser@gmail.com>
2018-07-05 18:11:58 -0400

    Firebase Testing

--- assets/js/firebase.js
+++ assets/js/firebase.js
@@ -42,4 +42,22 @@
               removal[this.value] = true;
              })
       });
+
+
+//       dataRef.remove()
+//         .then(function() {
+//           console.log("Remove succeeded.")
+//         })
+//         .catch(function(error) {
+//           console.log("Remove failed: " + error.message)
+//         });
+// }
+//   function removing(){
+// var gameName = firebase.database().ref(gameID);
+// var query = gameName.orderByChild('name').equalTo(name);
+// query.on('child_added', function(snapshot) {
+//     snapshot.ref.remove();
+// })
+
+    // https://firebase.google.com/docs/reference/js/firebase.database.Reference#remove
         //  <span> <a href="#" class='wishlist' data-name=${steamPrevious} data-appid=${searchedID}> ${gameID} </a></span>`);
